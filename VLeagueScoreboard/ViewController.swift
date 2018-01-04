61c4fbc510ced5a780c64c313212722a7fbffc51
Arofando, Hadi <s3618954@s00204044n-18.rmit.edu.vn>
2018-01-04 20:50:53 +0700

    Initial Commit

---
+++ VLeagueScoreboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VLeagueScoreboard
+//
+//  Created by Arofando, Hadi  on 1/4/18.
+//  Copyright © 2018 VLA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
